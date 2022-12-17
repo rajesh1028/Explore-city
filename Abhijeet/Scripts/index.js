6ed5e9073577bbc2aa98790392205ab0642b108a
Abhijeet Hiwale <abhijeethiwalemail@gmail.com>
2022-12-17 18:09:04 +0530

    City Name added

--- Abhijeet/Scripts/index.js
+++ Abhijeet/Scripts/index.js
@@ -35,3 +35,46 @@ function myDropdownINR() {
       }
     }
   }
+
+  let Data_for_tripsvilla;
+async function fetchData() {
+  try {
+    let res = await fetch("https://gold-salamander-shoe.cyclic.app/hotels");
+    let data = await res.json();
+    Data_for_tripsvilla = data
+    // console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function selectCity(city) {
+  console.log(city);
+  let data_city = Data_for_tripsvilla.filter((el) => {
+    return el.city == city;
+  });
+
+  //  THIS KEY IS WHAT YOU HAVE TO USE IN YOUR FILE
+  localStorage.setItem("data_city", JSON.stringify(data_city));
+  window.location.href = "./second.html";
+}
+
+// console.log(Data_for_tripsvilla)
+// document
+//   .getElementById("submit_search_button")
+//   .addEventListener("click", () => {
+//     fetchData();
+//     let city = document.querySelector("#location").value;
+//     let data_city = Data_for_tripsvilla.filter((el) => {
+//       return el.city == city;
+//     });
+//     localStorage.setItem("data_city", JSON.stringify(data_city));
+//   });
+
+
+  function getCity(id){
+    let name = id.split("_");
+    // console.log(name[0]);
+    localStorage.setItem('city_Name',name[0])
+    window.location.href = "../Punit/homesPage.html"
+  }
