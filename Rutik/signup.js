361bd9aebae07db0027fd263f0b12f396deb4acd
Rutikkharwade <rutikkharwade85@gmail.com>
2022-12-18 13:43:21 +0530

    add

---
+++ Rutik/signup.js
@@ -0,0 +1,65 @@
+let infoArr=JSON.parse(localStorage.getItem("info"))||[];
+
+document.querySelector("form").addEventListener("submit",function(event){
+    event.preventDefault();
+    let Email=document.querySelector("#email").value;
+    let mono=document.querySelector("#num").value;
+    let passward=document.querySelector("#pass").value;
+    let username=document.querySelector("#username").value;
+    let FirstName=document.querySelector("#userFName").value;
+    let LastName=document.querySelector("#userLName").value;
+    let id= infoArr.length;
+    let obj={
+        Email,
+        mono,
+        passward,
+        username,
+        FirstName,
+        LastName,
+        id,
+    }
+    
+    if(obj.Email==""){
+        alert("enter email")
+    }
+    else if(obj.mono.length>10||obj.mono.length<10){
+        alert("Please enter correct mobile number")
+    }else if(infoArr.length>0){ 
+     let msg="false"
+     infoArr.forEach(function(el,i,array){
+        
+        if(el.Email==obj.Email){
+            alert("Email is already exist")
+            msg="false"
+           
+        }else if(el.mono==obj.mono){
+            alert("Mobile number is already exist")
+            msg="false"
+            
+        }else{
+            msg="true"
+        }
+       
+     })
+         if(msg=="true"){
+            infoArr.push(obj);
+            alert("sign up succesful")
+            window.location.href="./login.html"
+         }else{
+            alert("enter correct details")
+           
+         }
+         
+    }else{
+        infoArr.push(obj);
+        alert("sign up succesful")
+    }
+
+    localStorage.setItem("info",JSON.stringify(infoArr));
+})
+
+
+
+
+
+
