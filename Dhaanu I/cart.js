5e0dfd9a97d8ae6069722ddd68bc61006ed4cd76
DhaanuI <dhaanu97@gmail.com>
2022-12-19 02:29:38 +0530

    day5

--- Dhaanu I/cart.js
+++ Dhaanu I/cart.js
@@ -57,5 +57,107 @@ function paymentfun()
 }
 
 
+let RefID=sessionStorage.getItem("refid");
+
+let information=JSON.parse(sessionStorage.getItem("dataObject"));
+console.log(information,information["checkindate"])
+
+async function getData()
+{
+    try{
+        let fetchData=await fetch(`https://gold-salamander-shoe.cyclic.app/hotels?refid=${RefID}`)
+
+        let data = await fetchData.json();
+        display(data);
+
+    }
+    catch (err) {
+        alert("something went wrong");
+      }
+}
+getData();
+
+function display(data){
+    let selectedImage = document.querySelector("#image");
+    let arr=[];
+    console.log(data)
+    arr.push(`<img id="cartimage"src="${data[0].img_url}" alt=""></img>`)
+    arr = arr.join("");
+    selectedImage.innerHTML = arr;
+
+    let selected=document.querySelector("#selectedinfo");
+    let array=[];
+    array.push(
+        `<h4 id="cartinfo">${data[0].title}</h4>`
+    )
+    array.push(
+        `<p id="country">${data[0].city}  ${data[0].country}</p>`
+    )
+    array.push(
+        `<p id="completeinfo" >${data[0].type_of_property} | Accommodates max ${data[0].maxguests} guests | <br>
+        ${data[0].bathroom} Bathroom(s) | ${data[0].bedroom} Bedroom(s)  </p>`
+    )
+   
+    array=array.join("")
+    selected.innerHTML = array;
+
+    let checkin = document.querySelector("#checkin");
+    let checkout = document.querySelector("#checkout");
+    let guests = document.querySelector("#num");
+    let units = document.querySelector("#units");
+
+    let date=[];
+    date.push(
+        `<h4 >${information.checkindate}</h4>`
+    )
+    date.push(
+        `<h4 >Check In</h4>`
+    )
+    date=date.join("")
+    checkin.innerHTML = date;
+    date=[];
+    date.push(
+        `<h4 >${information.checkout}</h4>`
+    )
+    date.push(
+        `<h4 >Check Out</h4>`
+    )
+    date=date.join("")
+    checkout.innerHTML = date;
+    date=[];
+    date.push(
+        `<h4 >Guests</h4>`
+    )
+    date.push(
+        `<h4 >${information.guests}</h4>`
+    )
+   
+    date=date.join("")
+    guests.innerHTML = date;
+    date=[];
+    date.push(
+        `<h4 >1</h4>`
+    )
+    date.push(
+        `<h4 id="unit">Units</h4>`
+    )
+    units.innerHTML = date;
+
+    let subtotal=document.querySelector("#subtotal");
+    let discount=document.querySelector("#discount");
+    let tax=document.querySelector("#tax");
+    let total=document.querySelector("#total");
+    let value=information.total;
+    let taxvalue=(value*10)/100;
+    totalamount=value+taxvalue;
+    subtotal.innerText=`Sub Total ........................................................................................................................................  Rs${information.total}`
+    discount.innerText=`Discount ...................................................................................................................................................  0`
+    tax.innerText=`Tax ........................................................................................................................................................ Rs${taxvalue}`
+    total.innerText=`Total ................................................................................................................... Rs ${totalamount}`
+
+
+
+}
+
 
 
